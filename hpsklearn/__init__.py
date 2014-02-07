b73d73d070a4b898bb0104cd62921e5218d2314d
James Bergstra <james.bergstra@gmail.com>
2014-02-06 19:34:07 -0500

    warn about potential openblas bug we trigger

---
+++ hpsklearn/__init__.py
@@ -0,0 +1,6 @@
+import os
+OMP_NUM_THREADS = os.environ.get('OMP_NUM_THREADS', None)
+if OMP_NUM_THREADS != 1:
+    print ('WARN: if you are using openblas set OMP_NUM_THREADS=1'
+           ' or risk subprocess calls hanging indefinitely')
+
