f63f3fb684168dfac961a7bf6d240d65fb49cd74
James Bergstra <james.bergstra@gmail.com>
2014-02-13 01:47:29 -0500

    added module symbol hyperopt_estimator

--- hpsklearn/__init__.py
+++ hpsklearn/__init__.py
@@ -4,3 +4,6 @@ if OMP_NUM_THREADS != 1:
     print ('WARN: if you are using openblas set OMP_NUM_THREADS=1'
            ' or risk subprocess calls hanging indefinitely')
 
+from estimator import hyperopt_estimator
+
+# -- flake8
