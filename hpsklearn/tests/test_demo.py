7b31003c2a99a446979280051a8c1917429c10bb
Li Shen <shenli.sam@gmail.com>
2016-08-08 16:33:00 -0400

    Add back test_demo.py
    * Add back test_demo.py and change the Iris dataset from skdata
      to sklearn.
    * Change the display of the loss for each trial from graphical to
      textual output.

---
+++ hpsklearn/tests/test_demo.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+# import numpy as np
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from hyperopt import tpe
+import hpsklearn
+import sys
+
+def test_demo_iris():
+
+    iris = datasets.load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(
+        iris.data, iris.target, test_size=.25, random_state=1)
+
+    estimator = hpsklearn.HyperoptEstimator(
+        preprocessing=hpsklearn.components.any_preprocessing('pp'),
+        classifier=hpsklearn.components.any_classifier('clf'),
+        algo=tpe.suggest,
+        trial_timeout=15.0,  # seconds
+        max_evals=10,
+        seed=1
+    )
+
+    # /BEGIN `Demo version of estimator.fit()`
+
+    iterator = estimator.fit_iter(X_train, y_train)
+    next(iterator)
+
+    n_trial = 0
+    while len(estimator.trials.trials) < estimator.max_evals:
+        iterator.send(1)  # -- try one more model
+        n_trial += 1
+        print('Trial', n_trial, 'loss:', estimator.trials.losses()[-1], 
+              file=sys.stderr)
+        # hpsklearn.demo_support.scatter_error_vs_time(estimator)
+        # hpsklearn.demo_support.bar_classifier_choice(estimator)
+
+    estimator.retrain_best_model_on_full_data(X_train, y_train)
+
+    # /END Demo version of `estimator.fit()`
+
+    print('Test accuracy:', estimator.score(X_test, y_test), file=sys.stderr)
