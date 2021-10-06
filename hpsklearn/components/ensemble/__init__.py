1a12524b8f5c1bfe66d993e7f3b4e261cc8c175f
mandjevant <38689620+mandjevant@users.noreply.github.com>
2021-10-06 16:24:04 +0200

    Add relative import to ensemble.__init__

--- hpsklearn/components/ensemble/__init__.py
+++ hpsklearn/components/ensemble/__init__.py
@@ -3,3 +3,7 @@ from ._forest import \
     random_forest_regressor, \
     extra_trees_classifier, \
     extra_trees_regressor
+
+from ._bagging import \
+    bagging_classifier, \
+    bagging_regressor
