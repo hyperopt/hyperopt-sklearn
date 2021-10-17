2a66b41c7c18e663a828e3e38843c090b5f88883
mandjevant <38689620+mandjevant@users.noreply.github.com>
2021-10-17 10:56:23 +0200

    Lars, LassoLars, LarsCV, LassoLarsCV, LassoLarsIC

---
+++ hpsklearn/components/linear_model/_least_angle.py
@@ -0,0 +1,335 @@
+from hpsklearn.components._base import validate
+
+from hyperopt.pyll import scope
+from hyperopt import hp
+
+from sklearn import linear_model
+import numpy.typing as npt
+import numpy as np
+import typing
+
+
+@scope.define
+def sklearn_Lars(*args, **kwargs):
+    return linear_model.Lars(*args, **kwargs)
+
+
+@scope.define
+def sklearn_LassoLars(*args, **kwargs):
+    return linear_model.LassoLars(*args, **kwargs)
+
+
+@scope.define
+def sklearn_LarsCV(*args, **kwargs):
+    return linear_model.LarsCV(*args, **kwargs)
+
+
+@scope.define
+def sklearn_LassoLarsCV(*args, **kwargs):
+    return linear_model.LassoLarsCV(*args, **kwargs)
+
+
+@scope.define
+def sklearn_LassoLarsIC(*args, **kwargs):
+    return linear_model.LassoLarsIC(*args, **kwargs)
+
+
+def _least_angle_n_nonzero_coefs(name: str):
+    """
+    Declaration search space 'n_nonzero_coefs' parameter
+    """
+    return hp.qloguniform(name, low=np.log(400), high=np.log(600), q=1.0)
+
+
+def _least_angle_alpha(name: str):
+    """
+    Declaration search space 'alpha' parameter
+    """
+    return hp.normal(name, mu=1.0, sigma=.1)
+
+
+def _least_angle_max_iter(name: str):
+    """
+    Declaration search space 'max_iter' parameter
+    """
+    return hp.qloguniform(name, low=np.log(250), high=np.log(750), q=1.0)
+
+
+def _least_angle_cv(name: str):
+    """
+    Declaration search space 'cv' parameter
+    """
+    return hp.pchoice(name, [
+        # create custom distribution
+        (0.0625, 3),
+        (0.175, 4),
+        (0.525, 5),
+        (0.175, 6),
+        (0.0625, 7),
+    ])
+
+
+def _least_angle_criterion(name: str):
+    """
+    Declaration search space 'criterion' parameter
+    """
+    return hp.choice(name, ["bic", "aic"])
+
+
+def _least_angle_max_n_alphas(name: str):
+    """
+    Declaration search space 'max_n_alphas' parameter
+    """
+    return hp.loguniform(name, low=np.log(750), high=np.log(1250))
+
+
+def _least_angle_random_state(name: str):
+    """
+    Declaration search space 'random_state' parameter
+    """
+    return hp.randint(name, 5)
+
+
+def _least_angle_hp_space(
+        name_func,
+        fit_intercept: bool = True,
+        verbose: typing.Union[bool, int] = False,
+        precompute: typing.Union[bool, str, npt.ArrayLike] = "auto",
+        eps: float = np.finfo(float).eps,
+        copy_X: bool = True,
+):
+    """
+    Hyper parameter of search space of common parameters for
+     lars
+     lasso lars
+     lars cv
+     lasso lars cv
+     lasso lars ic
+    """
+    hp_space = dict(
+        fit_intercept=fit_intercept,
+        verbose=verbose,
+        precompute=precompute,
+        eps=eps,
+        copy_X=copy_X
+    )
+    return hp_space
+
+
+def _least_angle_cv_shared_space(
+        hp_space: dict,
+        _name: callable,
+        max_iter: int = None,
+        cv: typing.Union[int, callable, typing.Generator] = None,
+        max_n_alphas: int = None,
+        n_jobs: int = None,
+):
+    """
+    Declaration shared search space parameters for
+     lars cv
+     lasso lars cv
+    """
+    hp_space["max_iter"] = max_iter or _least_angle_max_iter(_name("max_iter"))
+    hp_space["cv"] = cv or _least_angle_cv(_name("cv"))
+    hp_space["max_n_alphas"] = max_n_alphas or _least_angle_max_n_alphas(_name("max_n_alphas"))
+    hp_space["n_jobs"] = n_jobs
+    return hp_space
+
+
+@validate(params=["n_nonzero_coefs"],
+          validation_test=lambda param: isinstance(param, int) and param > 1,
+          msg="Invalid parameter '%s' with value '%s'. Parameter value must exceed 1.")
+def lars(name: str,
+         n_nonzero_coefs: int = None,
+         fit_path: bool = True,
+         jitter: float = None,
+         random_state=None,
+         **kwargs
+         ):
+    """
+    Return a pyll graph with hyperparameters that will construct
+    a sklearn.linear_model.Lars model.
+
+    Args:
+        name: name | str
+        n_nonzero_coefs: target number non-zero coefficients | int
+        fit_path: store full path | bool
+        jitter: upper bound noise parameter for stability | float
+        random_state: random number generation for jitter
+
+    See help(hpsklearn.components.linear_model._least_angle._least_angle_hp_space)
+    for info on additional available least angle arguments.
+    """
+
+    def _name(msg):
+        return f"{name}.lars_{msg}"
+
+    hp_space = _least_angle_hp_space(_name, **kwargs)
+    hp_space["n_nonzero_coefs"] = n_nonzero_coefs or _least_angle_n_nonzero_coefs(_name("n_nonzero_coefs"))
+    hp_space["fit_path"] = fit_path
+    hp_space["jitter"] = jitter  # highly dependent on data
+    hp_space["random_state"] = random_state or _least_angle_random_state(_name("random_state"))
+
+    return scope.sklearn_Lars(**hp_space)
+
+
+@validate(params=["alpha"],
+          validation_test=lambda param: isinstance(param, int) and param > 1,
+          msg="Invalid parameter '%s' with value '%s'. Alpha = 0 is equivalent to ordinary least square. "
+              "For ordinary least square, use LinearRegression instead.")
+@validate(params=["max_iter"],
+          validation_test=lambda param: isinstance(param, int) and param > 1,
+          msg="Invalid parameter '%s' with value '%s'. Parameter value must exceed 1.")
+def lasso_lars(name: str,
+               alpha: float = None,
+               max_iter: int = None,
+               fit_path: bool = True,
+               positive: bool = False,
+               jitter: float = None,
+               random_state=None,
+               **kwargs
+               ):
+    """
+    Return a pyll graph with hyperparameters that will construct
+    a sklearn.linear_model.LassoLars model.
+
+    Args:
+        name: name | str
+        alpha: constant that multiplies penalty | float
+        max_iter: maximum number of iterations | int
+        fit_path: store full path | bool
+        positive: restrict coefficient to positive | bool
+        jitter: upper bound noise parameter for stability | float
+        random_state: random number generation for jitter
+
+    See help(hpsklearn.components.linear_model._least_angle._least_angle_hp_space)
+    for info on additional available least angle arguments.
+    """
+
+    def _name(msg):
+        return f"{name}.lasso_lars_{msg}"
+
+    hp_space = _least_angle_hp_space(_name, **kwargs)
+    hp_space["alpha"] = alpha or _least_angle_alpha(_name("alpha"))
+    hp_space["max_iter"] = max_iter or _least_angle_max_iter(_name("max_iter"))
+    hp_space["fit_path"] = fit_path
+    hp_space["positive"] = positive
+    hp_space["jitter"] = jitter  # highly dependent on data
+    hp_space["random_state"] = random_state or _least_angle_random_state(_name("random_state"))
+
+    return scope.sklearn_LassoLars(**hp_space)
+
+
+@validate(params=["max_iter", "max_n_alphas"],
+          validation_test=lambda param: isinstance(param, int) and param > 0,
+          msg="Invalid parameter '%s' with value '%s'. Parameter value must exceed 0.")
+def lars_cv(name: str,
+            max_iter: int = None,
+            cv: typing.Union[int, callable, typing.Generator] = None,
+            max_n_alphas: int = None,
+            n_jobs: int = None,
+            **kwargs
+            ):
+    """
+    Return a pyll graph with hyperparameters that will construct
+    a sklearn.linear_model.LarsCV model.
+
+    Args:
+        name: name | str
+        max_iter: maximum number of iterations | int
+        cv: cross-validation splitting strategy | int, callable or generator
+        max_n_alphas: max number of points to compute residuals | int
+        n_jobs: number of CPUs to use during CV | int
+
+
+    See help(hpsklearn.components.linear_model._least_angle._least_angle_hp_space)
+    for info on additional available least angle arguments.
+    """
+
+    def _name(msg):
+        return f"{name}.lars_cv_{msg}"
+
+    hp_space = _least_angle_hp_space(_name, **kwargs)
+    hp_space = _least_angle_cv_shared_space(hp_space=hp_space, _name=_name,
+                                            max_iter=max_iter, cv=cv,
+                                            max_n_alphas=max_n_alphas,
+                                            n_jobs=n_jobs)
+
+    return scope.sklearn_LarsCV(**hp_space)
+
+
+@validate(params=["max_iter", "max_n_alphas"],
+          validation_test=lambda param: isinstance(param, int) and param > 0,
+          msg="Invalid parameter '%s' with value '%s'. Parameter value must exceed 0.")
+def lasso_lars_cv(name: str,
+                  max_iter: int = None,
+                  cv: typing.Union[int, callable, typing.Generator] = None,
+                  max_n_alphas: int = None,
+                  n_jobs: int = None,
+                  positive: bool = False,
+                  **kwargs
+                  ):
+    """
+    Return a pyll graph with hyperparameters that will construct
+    a sklearn.linear_model.LassoLarsCV model.
+
+    Args:
+        name: name | str
+        max_iter: maximum number of iterations | int
+        cv: cross-validation splitting strategy | int, callable or generator
+        max_n_alphas: max number of points to compute residuals | int
+        n_jobs: number of CPUs to use during CV | int
+        positive: restrict coefficient to positive | bool
+
+    See help(hpsklearn.components.linear_model._least_angle._least_angle_hp_space)
+    for info on additional available least angle arguments.
+    """
+
+    def _name(msg):
+        return f"{name}.lasso_lars_cv_{msg}"
+
+    hp_space = _least_angle_hp_space(_name, **kwargs)
+    hp_space = _least_angle_cv_shared_space(hp_space=hp_space, _name=_name,
+                                            max_iter=max_iter, cv=cv,
+                                            max_n_alphas=max_n_alphas,
+                                            n_jobs=n_jobs)
+    hp_space["positive"] = positive
+
+    return scope.sklearn_LassoLarsCV(**hp_space)
+
+
+@validate(params=["criterion"],
+          validation_test=lambda param: isinstance(param, str) and param in ("bic", "aic"),
+          msg="Invalid parameter '%s' with value '%s'. Choose 'bic' or 'aic'.'")
+@validate(params=["max_iter"],
+          validation_test=lambda param: isinstance(param, int) and param > 0,
+          msg="Invalid parameter '%s' with value '%s'. Parameter value must exceed 0.")
+def lasso_lars_ic(name: str,
+                  criterion: str = None,
+                  max_iter: int = None,
+                  positive: bool = False,
+                  **kwargs
+                  ):
+    """
+    Return a pyll graph with hyperparameters that will construct
+    a sklearn.linear_model.LassoLarsIC model.
+
+    Args:
+        name: name | str
+        criterion: choose from 'bic', 'aic' | str
+        max_iter: maximum number of iterations | int
+        positive: restrict coefficient to positive | bool
+
+    See help(hpsklearn.components.linear_model._least_angle._least_angle_hp_space)
+    for info on additional available least angle arguments.
+    """
+
+    def _name(msg):
+        return f"{name}.lasso_lars_ic_{msg}"
+
+    hp_space = _least_angle_hp_space(_name, **kwargs)
+    hp_space["criterion"] = criterion or _least_angle_criterion(_name("criterion"))
+    hp_space["max_iter"] = max_iter or _least_angle_max_iter(_name("max_iter"))
+    hp_space["positive"] = positive
+
+    return scope.sklearn_LassoLarsIC(**hp_space)
