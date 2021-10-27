ba31eb30c5202bce173a45edd6d89300b101bf9e
mandjevant <38689620+mandjevant@users.noreply.github.com>
2021-10-27 15:32:54 +0200

    Standard class for multi task regressor testing in utils

--- hpsklearn/tests/utils.py
+++ hpsklearn/tests/utils.py
@@ -38,6 +38,22 @@ class StandardRegressorTest(unittest.TestCase):
         self.Y_test = self.X_test[:, 0] * 2
 
 
+class MultiTaskRegressorTest(unittest.TestCase):
+    """
+    Standard class for multi task regressor testing
+    """
+
+    def setUp(self):
+        """
+        Setup of randomly generated data
+        """
+        np.random.seed(123)
+        self.X_train = np.random.randn(1000, 2)
+        self.Y_train = self.X_train * 2
+        self.X_test = np.random.randn(1000, 2)
+        self.Y_test = self.X_test * 2
+
+
 class StandardPreprocessingTest(StandardClassifierTest):
     """
     Standard class for preprocessing testing
