470d591fefa7495fb0636215132cc6671f52842e
mandjevant <38689620+mandjevant@users.noreply.github.com>
2021-11-13 17:16:21 +0100

    gaussian process clf and regressor

---
+++ hpsklearn/components/gaussian_process/__init__.py
@@ -0,0 +1,2 @@
+from ._gpc import gaussian_process_classifier
+from ._gpr import gaussian_process_regressor
---
+++ hpsklearn/components/gaussian_process/_gpr.py
@@ -0,0 +1,55 @@
+from hpsklearn.components._base import validate
+
+from hyperopt.pyll import scope
+from hyperopt import hp
+
+from sklearn import gaussian_process
+import numpy as np
+import typing
+
+
+@scope.define
+def sklearn_GaussianProcessRegressor(*args, **kwargs):
+    return gaussian_process.GaussianProcessRegressor(*args, **kwargs)
+
+
+@validate(params=["optimizer"],
+          validation_test=lambda param: isinstance(param, str) and param == "fmin_l_bfgs_b",
+          msg="Invalid parameter '%s' with value '%s'. Value must be 'fmin_l_bfgs_b' or callable.")
+def gaussian_process_regressor(name: str,
+                               kernel=None,
+                               alpha: typing.Union[float, np.ndarray] = None,
+                               optimizer: typing.Union[str, callable] = None,
+                               n_restarts_optimizer: int = None,
+                               normalize_y: bool = False,
+                               copy_X_train: bool = True,
+                               random_state=None):
+    """
+    Return a pyll graph with hyperparameters that will construct
+    a sklearn.gaussian_process.GaussianProcessRegressor model.
+
+    Args:
+        name: name | str
+        kernel: kernel instance
+        alpha: add value to diagonal of kernel matrix during fitting | float, np.ndarray
+        optimizer: optimizer for kernel parameter optimization | str, callable
+        n_restarts_optimizer: number of restarts optimizer | int
+        normalize_y: normalize target values | bool
+        copy_X_train: store persistent copy of training data | bool
+        random_state: random seed for center initialization | int
+    """
+    def _name(msg):
+        return f"{name}.gaussian_process_regressor_{msg}"
+
+    hp_space = dict(
+        kernel=kernel,
+        alpha=hp.loguniform(_name("alpha"), np.log(1e-10), np.log(1e-2)) if alpha is None else alpha,
+        optimizer="fmin_l_bfgs_b" if optimizer is None else optimizer,
+        n_restarts_optimizer=hp.pchoice(_name("n_restarts_optimizer"), [(0.5, 0), (0.10, 1), (0.10, 2), (0.10, 3),
+                                                                        (0.10, 4), (0.10, 5)])
+        if n_restarts_optimizer is None else n_restarts_optimizer,
+        normalize_y=normalize_y,
+        copy_X_train=copy_X_train,
+        random_state=hp.randint(_name("random_state"), 5) if random_state is None else random_state,
+    )
+    return scope.sklearn_GaussianProcessRegressor(**hp_space)
