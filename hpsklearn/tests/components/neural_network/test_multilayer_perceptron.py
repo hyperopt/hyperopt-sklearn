618864ec56a8d3aeaa2e7cbd327d09b56519f4ad
mandjevant <38689620+mandjevant@users.noreply.github.com>
2021-11-14 14:11:12 +0100

    mlp clf and regression tests

---
+++ hpsklearn/tests/components/neural_network/test_multilayer_perceptron.py
@@ -0,0 +1,39 @@
+import unittest
+
+from hpsklearn import \
+    mlp_classifier, \
+    mlp_regressor
+from hpsklearn.tests.utils import \
+    StandardRegressorTest, \
+    StandardClassifierTest, \
+    generate_test_attributes
+
+
+class TestMLPClassifier(StandardClassifierTest):
+    """
+    Class for _multilayer_perceptron classification testing
+    """
+
+
+class TestMLPRegression(StandardRegressorTest):
+    """
+    Class for _multilayer_perceptron regression testing
+    """
+
+
+generate_test_attributes(
+    TestClass=TestMLPClassifier,
+    fn_list=[mlp_classifier],
+    is_classif=True,
+)
+
+
+generate_test_attributes(
+    TestClass=TestMLPRegression,
+    fn_list=[mlp_regressor],
+    is_classif=False,
+)
+
+
+if __name__ == '__main__':
+    unittest.main()
