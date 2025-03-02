934341b9a9be7bbde8bdc6d15c1cc0ad34750e72
mandjevant <38689620+mandjevant@users.noreply.github.com>
2025-03-02 19:18:50 +0100

    Add unittests for hashing vectorizer and count vectorizer. Use SGD classifier since multinomial_nb does not accept negative values in hashing vectorizer

--- tests/test_components/test_feature_extraction/test_text.py
+++ tests/test_components/test_feature_extraction/test_text.py
@@ -2,8 +2,11 @@ import unittest
 
 from hpsklearn import \
     HyperoptEstimator, \
-    tfidf, \
-    multinomial_nb
+    multinomial_nb, \
+    sgd_classifier, \
+    tfidf_vectorizer, \
+    hashing_vectorizer, \
+    count_vectorizer
 from tests.utils import TrialsExceptionHandler
 from hyperopt import rand
 
@@ -36,7 +39,7 @@ class TestTfidf(unittest.TestCase):
         """
         model = HyperoptEstimator(
             classifier=multinomial_nb("classifier"),
-            preprocessing=[tfidf("preprocessing")],
+            preprocessing=[tfidf_vectorizer("preprocessing")],
             algo=rand.suggest,
             trial_timeout=10.0,
             max_evals=5,
@@ -44,7 +47,41 @@ class TestTfidf(unittest.TestCase):
         model.fit(self.X_test, self.Y_test)
         model.score(self.X_test, self.Y_test)
 
-    test_tfidf_vectorizer.__name__ = f"test_{tfidf.__name__}"
+    @TrialsExceptionHandler
+    def test_hashing_vectorizer(self):
+        """
+        Instantiate sgd_classifier hyperopt_estimator model
+         add HashingVectorizer preprocessor
+         fit and score model
+        """
+        model = HyperoptEstimator(
+            classifier=sgd_classifier("classifier"),
+            preprocessing=[hashing_vectorizer("preprocessing")],
+            algo=rand.suggest,
+            trial_timeout=10.0,
+            max_evals=5,
+        )
+        model.fit(self.X_test, self.Y_test)
+    
+    @TrialsExceptionHandler
+    def test_count_vectorizer(self):
+        """
+        Instantiate multinomial_nb hyperopt_estimator model
+         add CountVectorizer preprocessor
+         fit and score model
+        """
+        model = HyperoptEstimator(
+            classifier=multinomial_nb("classifier"),
+            preprocessing=[count_vectorizer("preprocessing")],
+            algo=rand.suggest,
+            trial_timeout=10.0,
+            max_evals=5,
+        )
+        model.fit(self.X_test, self.Y_test)
+
+    test_tfidf_vectorizer.__name__ = f"test_{tfidf_vectorizer.__name__}"
+    test_hashing_vectorizer.__name__ = f"test_{hashing_vectorizer.__name__}"
+    test_count_vectorizer.__name__ = f"test_{count_vectorizer.__name__}"
 
 
 if __name__ == "__main__":
